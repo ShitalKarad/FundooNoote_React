b7565f7c5ec4d33c7ad897603f9f6d8374bd9fe0
ShitalKarad <shitalkarad24@gmail.com>
2023-09-21 16:49:53 +0530

    Modified:login page

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Grid from '@mui/material/Grid'
-import { Typography, TextField, Container } from '@mui/material'
+import { Typography, TextField, Container,Link,Button } from '@mui/material'
 
 
 function Login() {
@@ -12,12 +12,12 @@ function Login() {
                 <Grid container sx={{
                     border: { xs: '1px solid #f3f3f3', sm: '1px solid red', md: '1px solid blue' },
                     width: '400px', height: 'auto', boxShadow: "5px", alignItems: "center", padding: '20px', gap: '20px',
-                    display:'flex', flexDirection:'column' , alignItems:'center'
-                   
-                }}>
-                    <Grid item style={{ marginTop: '0px', }}>
+                    display: 'flex', flexDirection: 'column', alignItems: 'center'
 
-                        <div style={{ display: 'flex' }}>
+                }}>
+                    <Grid item style={{ marginTop: '0px' }}>
+
+                        <div style={{ display: 'flex' , justifyContent:'center' }}>
                             {googleText.split('').map((letter, index) => (
                                 <Typography key={index} variant="h2" color="" component='p' style={{ color: colors[index], fontSize: '25px' }}>
                                     {letter}
@@ -25,13 +25,29 @@ function Login() {
                             ))}
                         </div>
 
-                        <h1 style={{ fontFamily: 'Roboto, sans-serif', fontSize: '1.0rem',textAlign:'center' }}>Login</h1>
-                        <h1 style={{ fontFamily: 'Roboto, sans-serif', fontSize: '1.0rem',textAlign:'center' }}> Use Your Fundoo acoount</h1>
+                        <h1 style={{ fontFamily: 'Roboto, sans-serif', fontSize: '1.0rem', textAlign: 'center' }}>Login</h1>
+                        <h1 style={{ fontFamily: 'Roboto, sans-serif', fontSize: '1.0rem', textAlign: 'center' }}> Use Your Fundoo acoount</h1>
                     </Grid>
-                    <Grid item container style={{ padding:'10px', marginBottom:"20px"}}>
-                    <TextField fullWidth label="fullWidth" id="fullWidth" />
-                    <TextField fullWidth label="fullWidth" id="fullWidth" />
+                    <Grid item container style={{ padding: '10px', marginBottom: "20px" }}>
+                        <TextField fullWidth label="fullWidth" id="fullWidth" />
+                        <TextField fullWidth label="fullWidth" id="fullWidth" />
+                                <p>Forgot Password</p>
+
                     </Grid>
+                    <Grid item >
+                        <Grid item xs={12} sm={9} sx={{ display: "flex", gap: 20 }}>
+                        <Typography>
+                                <Link href="#" color="primary">
+                                    <span style={{ whiteSpace: "nowrap" }}>Sign in instead</span>
+                                </Link>
+                            </Typography>
+
+                            <Button variant="contained" color="primary" >
+                                Next
+                            </Button>
+                        </Grid>
+                        </Grid>
+                            
                 </Grid>
             </Grid>
         </Container>
